9331a6296ce3a5d0777f78e87236e4d98b3a16fa
Arhur <truearche@gmail.com>
2026-06-12 16:27:24 +0400

    simplify setup.sh - env copying handled by worktreeinclude

--- .conductor/setup.sh
+++ .conductor/setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Копируем .env из основного репозитория
-cp /Users/arthur.leontev/Desktop/LMS/test2/.env .env
-
 # Добавляем уникальное имя проекта для изоляции Docker
 echo "COMPOSE_PROJECT_NAME=xyz_${CONDUCTOR_WORKSPACE_NAME:-dev}" >> .env
 
