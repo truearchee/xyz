e1f82892da6e3f2261a73fd1a01bd0ed333f1b7b
Arhur <truearche@gmail.com>
2026-06-13 20:12:16 +0400

    add explicit PATH to setup.sh for non-interactive shell

--- .conductor/setup.sh
+++ .conductor/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# Явно указываем PATH чтобы docker, npm и другие инструменты были доступны
+export PATH="$HOME/.local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:$PATH"
+
 # Добавляем уникальное имя проекта для изоляции Docker
 echo "COMPOSE_PROJECT_NAME=xyz_${CONDUCTOR_WORKSPACE_NAME:-dev}" >> .env
 
